56ff58c89138370811d0e2cea33d8f0b3f2eaf2f
Thiago Zagui Giacomini <thiagozg1995@gmail.com>
2020-06-07 15:59:55 -0300

    Added default dev interceptors

--- src/github_jobs/di/http_config.clj
+++ src/github_jobs/di/http_config.clj
@@ -1,11 +1,15 @@
 (ns github-jobs.di.http-config
   (:require [github-jobs.service :as service]
-            [io.pedestal.http :as http]))
+            [io.pedestal.http :as http]
+            [io.pedestal.http :as server]))
 
 (defn provides
   [environment]
-  {:env               environment
-   ::http/routes service/routes
-   ::http/type   :jetty
-   ::http/port   8890
-   ::http/join?  false})
+  (-> {:env                 environment
+       ::http/routes        service/routes
+       ::http/type          :jetty
+       ::http/port          8890
+       ::http/resource-path "/public"
+       ::http/join?         false}
+      server/default-interceptors
+      server/dev-interceptors))
