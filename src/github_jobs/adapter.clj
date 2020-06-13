877e55ac2e7f24f82d95985c9e3354725ef4df67
Thiago Zagui Giacomini <thiagozg1995@gmail.com>
2020-06-12 21:29:18 -0300

    Makes adapter to use coerce instead of validate

--- src/github_jobs/adapter.clj
+++ src/github_jobs/adapter.clj
@@ -1,7 +1,15 @@
 (ns github-jobs.adapter
-  (:require [io.pedestal.interceptor :as i]
-            [schema.core :as s]
+  (:require [schema.utils :as s-utils]
+            [schema.coerce :as coerce]
             [io.pedestal.interceptor.helpers :as interceptor]))
+
+(defn- coerce-and-validate! [schema matcher data]
+  (let [coercer (coerce/coercer schema matcher)
+        result (coercer data)]
+    (if (s-utils/error? result)
+      (throw (IllegalArgumentException. (format "Value does not match schema: %s"
+                                                (s-utils/error-val result))))
+      result)))
 
 (defn coerce-body-request
   [schema]
@@ -9,6 +17,6 @@
                           (fn [request]
                             (dissoc
                               (->> (:json-params request)
-                                   (s/validate schema)
+                                   (coerce-and-validate! schema coerce/json-coercion-matcher)
                                    (assoc request :payload))
                               :json-params))))
