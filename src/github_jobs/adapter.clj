97da74ad245a03f12c97873fcd5a3c56f90c4575
Thiago Zagui Giacomini <thiagozg1995@gmail.com>
2020-06-27 13:23:07 -0300

    Moved service error handler to adapter

--- src/github_jobs/adapter.clj
+++ src/github_jobs/adapter.clj
@@ -1,7 +1,27 @@
 (ns github-jobs.adapter
   (:require [schema.utils :as s-utils]
             [schema.coerce :as coerce]
-            [io.pedestal.interceptor.helpers :as interceptor]))
+            [io.pedestal.interceptor.helpers :as interceptor]
+            [io.pedestal.interceptor.error :as error-int]
+            [taoensso.timbre :as timbre]))
+
+(defn- log-error
+  [ex function]
+  (do (timbre/error ex) function))
+
+(def service-error-handler
+  (error-int/error-dispatch
+    [context ex]
+
+    [{:exception-type :java.lang.IllegalArgumentException}]
+    (log-error
+      ex (assoc context :response {:status 412
+                                   :body   {:message "The request body does not match with contract"}}))
+
+    :else
+    (log-error
+      ex (assoc context :response {:status 500
+                                   :body   "Internal Server Error"}))))
 
 (defn- coerce-and-validate! [schema matcher data]
   (let [coercer (coerce/coercer schema matcher)
--- src/github_jobs/service.clj
+++ src/github_jobs/service.clj
@@ -2,12 +2,10 @@
   (:require [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
             [io.pedestal.http.body-params :as body-params]
-            [io.pedestal.interceptor.error :as error-int]
             [github-jobs.controller :as controller]
             [github-jobs.schemata-in.job :as s-in]
             [github-jobs.adapter :as adapter]
-            [ring.util.response :as ring-resp]
-            [taoensso.timbre :as timbre]))
+            [ring.util.response :as ring-resp]))
 
 (defn about-page
   [request]
@@ -21,27 +19,21 @@
   (controller/save-job-async payload (:conn datomic))
   {:status 201})
 
-(defn- log-error
-  [ex function]
-  (do (timbre/error ex) function))
+(defn update-job
+  [{:keys [path?? payload]
+    {:keys [datomic]} :context-deps}]
+  (controller/update-job-async payload (:conn datomic))
+  {:status 200})
 
-(def ^:private service-error-handler
-  (error-int/error-dispatch
-    [context ex]
-
-    [{:exception-type :java.lang.IllegalArgumentException}]
-    (log-error
-      ex (assoc context :response {:status 412
-                                   :body   {:message "The request body does not match with contract"}}))
-
-    :else
-    (log-error
-      ex (assoc context :response {:status 500
-                                   :body   "Internal Server Error"}))))
+(defn delete-job
+  [{:keys [path??]
+    {:keys [datomic]} :context-deps}]
+  (controller/update-job-async path?? (:conn datomic))
+  {:status 200})
 
 (def routes
   (route/expand-routes
-    `[[["/api" ^:interceptors [service-error-handler]
+    `[[["/api" ^:interceptors [adapter/service-error-handler]
 
         ; TODO: remove this later
         ["/about" ^:interceptors [(body-params/body-params)
@@ -52,5 +44,15 @@
                                 (adapter/coerce-body-request s-in/JobReference)
                                 http/json-body]
          {:post save-new-job}]
+
+        ["/job" ^:interceptors [(adapter/coerce-path-param ???)
+                                (body-params/body-params)
+                                (adapter/coerce-body-request s-in/JobReference)
+                                http/json-body]
+         {:put update-job}]
+
+        ["/job" ^:interceptors [(adapter/coerce-path-param ???)
+                                http/json-body]
+         {:delete delete-job}]
 
         ]]]))
