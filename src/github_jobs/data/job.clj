61e43888a3f81b35f4359ba62ca08fb2d5152c37
thiagozg <thiago.giacomini@nubank.com.br>
2020-07-04 17:49:19 -0300

    Create logic to update jobs

--- src/github_jobs/data/job.clj
+++ src/github_jobs/data/job.clj
@@ -1,8 +1,8 @@
 (ns github-jobs.data.job
   (:require [schema.core :as s]
-            [github-jobs.model.job :refer [JobDTO]]
-            [github-jobs.model.category :refer [CategoryDTO]]
-            [datomic.api :as d]))
+            [github-jobs.model.job :refer [NewDto UpdateDto]]
+            [datomic.api :as d]
+            ))
 
 (defn get-jobs!
   [conn
@@ -14,23 +14,30 @@
                              :where [[?job :job/id _]]}
                     :args  [(d/db conn)]}]
     (cond-> query-base
-            title (->
-                    (update-in [:query :in] conj '?title)
-                    (update-in [:query :where] conj '[?job :job/title ?title])
-                    (update-in [:args] conj title))
-            category (->
-                       (update-in [:query :in] conj '?category)
-                       (update-in [:query :where] conj '[?job :job/category ?category])
-                       (update-in [:args] conj category))
-            github-id (->
-                        (update-in [:query :in] conj '?github-id)
-                        (update-in [:query :where] conj '[?job :job/github-id ?github-id])
-                        (update-in [:args] conj github-id))
+
+            title
+            (->
+              (update-in [:query :in] conj '?title)
+              (update-in [:query :where] conj '[?job :job/title ?title])
+              (update-in [:args] conj title))
+
+            category
+            (->
+              (update-in [:query :in] conj '?category)
+              (update-in [:query :where] conj '[?job :job/category ?category])
+              (update-in [:args] conj category))
+
+            github-id
+            (->
+              (update-in [:query :in] conj '?github-id)
+              (update-in [:query :where] conj '[?job :job/github-id ?github-id])
+              (update-in [:args] conj github-id))
+
             true d/query)))
 
 (s/defn insert-job!
   [conn
-   job :- JobDTO]
+   job :- NewDto]
   (d/transact conn [job]))
 
 (s/defn find-job!
@@ -42,20 +49,45 @@
       (throw (IndexOutOfBoundsException.
                (format "This github id: %s was not founded!" github-id))))))
 
+(s/defn add-job-categories!
+  [conn
+   github-id :- s/Uuid
+   categories :- [s/Str]]
+  (println categories)
+  (for [category categories]
+    (d/transact conn [[:db/add [:job/github-id github-id] :job/category category]])))
+
+(defn ^:private update-job-cas-datoms!
+  [conn
+   github-id
+   old-datom
+   datom-key
+   new-datom]
+  (d/transact conn [[:db/cas [:job/github-id github-id] datom-key old-datom new-datom]]))
+
+(defn ^:private datom-key
+  [attribute]
+  (keyword "job" (name attribute)))
+
+(defn ^:private map-update-job
+  [old-job conn github-id]
+  (fn [[key value]]
+    (if-not (vector? value)
+      (update-job-cas-datoms! conn github-id (get old-job (datom-key key)) (datom-key key) value)
+      (add-job-categories! conn github-id value))))
+
 (s/defn update-job!
   [conn
    github-id :- s/Uuid
-   job :- JobDTO]
-  ;; TODO update with github-job-id
-  (d/transact conn [job]))
+   new-job :- UpdateDto]
+  (println "hue:" github-id)
+  (println "hue:" new-job)
+  (-> (find-job! conn github-id)
+      (map-update-job conn github-id)
+      (map new-job)))
 
 (s/defn retract-job!
   [conn
    github-id :- s/Uuid]
   (find-job! conn github-id)
   (d/transact conn [[:db/retractEntity [:job/github-id github-id]]]))
-
-(s/defn upsert-category!
-  [conn
-   category :- CategoryDTO]
-  (d/transact conn category))
